3cdbe2aad8992e2e4f3544b19bc5dcbcc7e0792d
carlos crispin <mantillajulian234@gmail.com>
2022-01-02 01:35:21 -0500

    de una base de datos, hago una busqueda y la renderizo en html

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response, redirect, render_template
+from data_workers import DATA
 
 fruits = ['banana', 'apple', 'orange', 'cherry']
 
@@ -13,14 +14,24 @@ def index():
 
     return response
 
+# Obtiene una lista de acuerdo a la condicion
+def get_list():
+    python_dev_list = list(filter(lambda worker : worker['language'] == 'python', DATA))
+    python_dev_list = list(map(lambda worker: worker['name'], python_dev_list))
+
+    return python_dev_list
+
 
 # se crea un diccionario con los parametros que vamos a pasar
 # la notacion **context, nos permite expandir el diccionario
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
+    python_dev_list = get_list()
+    
     context ={
         'user_ip':user_ip,
         'fruits':fruits,
+        'python_dev_list':python_dev_list,
     }
     return render_template('hello.html', **context )
