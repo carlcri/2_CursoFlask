9a688c3ff94fe226c041840d1e99fcbfef740f3f
carlos crispin <mantillajulian234@gmail.com>
2022-01-01 23:19:43 -0500

    basico templates

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# export FLASK_APP=main.py
-# export FLASK_DEBUG=1
-# flask run
+# se importa render_template
+from flask import Flask, request, make_response, redirect, render_template
 
 
-# se importa make_response, redirect
-from flask import Flask, request, make_response, redirect
-
-# se instancia un objeto de Flask, y se le da como parametro el nombre de la aplicacion que es main.py
 app = Flask(__name__)
 
 # ruta raiz
@@ -20,8 +15,9 @@ def index():
 
 
 
-# ruta hello
+# ruta hello, donde ahora en vez de retorna una cadena, retornamos el template: hello.html
+# tambien enviamos 
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return f'hello your ip is: {user_ip}'
+    return render_template('hello.html', user_ip=user_ip)
