136197ef4b6d192b0257c49be9cfa8cec208a861
carlos crispin <mantillajulian234@gmail.com>
2022-01-05 00:44:12 -0500

    despues de instalar bootstrap, herede en el base.html de bootstrap, implemente paginas de error

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, make_response, redirect, render_template
 from data_workers import DATA
+from flask_bootstrap import Bootstrap
 
 fruits = ['banana', 'apple', 'orange', 'cherry']
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -35,3 +37,17 @@ def hello():
         'python_dev_list':python_dev_list,
     }
     return render_template('hello.html', **context )
+
+
+@app.route('/error')
+def internal_error():
+    1/0
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
--- run_flask_server.sh
+++ run_flask_server.sh
@@ -2,5 +2,7 @@ source venv/bin/activate
 
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
+export FLASK_ENV=development
+echo FLASK_ENV
 
 flask run
