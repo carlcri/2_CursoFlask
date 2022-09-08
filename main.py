9a2e6d01d57c7e580311887674ef7d03e5289202
mi name <smily_face@domain.com>
2022-09-07 22:40:03 -0500

    agrego Procfile, modifico main.py

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from flask import Flask
 # se instancia un objeto de Flask, y se le da como parametro el nombre de la aplicacion que es main.py
 app = Flask(__name__)
 
+DEBUG = False
+PORT = 5000
+
 
 # con este decorador estamos dandole una ruta
 @app.route('/')
-def hello():
-    return 'Hello World Flask'
+def index():
+    return 'Hello World Flask Deploy'
+
+if __name__ == '__main__':
+    app.run(port=PORT, debug=DEBUG)
