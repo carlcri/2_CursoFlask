fef2bf321fdbcffe34faa4a9659e08e0f97090ee
mi name <smily_face@domain.com>
2022-09-08 23:24:04 -0500

    request simple: user_ip

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 # se instancia un objeto de Flask, y se le da como parametro el nombre de la aplicacion que es main.py
 app = Flask(__name__)
@@ -10,7 +10,8 @@ PORT = 5000
 # con este decorador estamos dandole una ruta
 @app.route('/')
 def index():
-    return 'Hello Amazing World!!, You are playing with fire. '
+    user_ip = request.remote_addr
+    return f'Hello your ip is: {user_ip}'
 
 if __name__ == '__main__':
     app.run(port=PORT, debug=DEBUG)
